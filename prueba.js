33b3447852c72147c0d8cdec537958310f066a78
jhancarlos <jhvasquez1996@gmail.com>
2021-11-22 13:19:43 -0500

    fue agregado el slider de imagenes

--- prueba.js
+++ prueba.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-  let images = document.querySelectorAll(".empresa-complemento .empresa img");
+  let images = document.querySelectorAll(".slider-js .imganes-js");
   let interval = 0;
  setInterval(()=>{
     hiddenImg()
@@ -19,25 +19,3 @@ window.addEventListener("load", function () {
     });
   }
 });
-
-window.addEventListener("load", function () {
-    let images = document.querySelectorAll(".complemento img");
-    let interval = 0;
-   setInterval(()=>{
-      hiddenImg()
-      if(interval>= images.length-1){
-          interval=0
-      }else{
-          interval++
-      }
-   },3000)
-    function hiddenImg() {
-      images.forEach((img, index) => {
-        if (index == interval) {
-          img.style.display = "block";
-        }else{
-          img.style.display = "none";
-        }
-      });
-    }
-  });
